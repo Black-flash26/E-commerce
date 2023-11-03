a5f2529affa715d6bd6a38c4b7da3f40e30413e1
SilverGrace26 <ayushsaraswat2615@gmail.com>
2023-11-03 12:09:56 +0530

    Front End added

---
+++ FrontEnd/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Slider from './components/Slider'
+import Footer from './components/Footer'
+
+const Home = () => {
+  return (
+    <div> 
+        <Navbar/>
+        <Slider/> 
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
---
+++ FrontEnd/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.freepngimg.com/thumb/model/26651-8-mens-fashion-file.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://meltmoon.com/wp-content/uploads/2019/04/Womens-White-Hoodie-8-1000x1422.jpg",
+      title: "WINTER FIESTA",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "ffffff",
+    },
+  ];
